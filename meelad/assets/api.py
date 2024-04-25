6cb7ba679f54b19477c10ae26c466db25625da0d
Sarra-arab <sarra.arab@ensia.edu.dz>
2024-04-25 05:22:57 +0100

    sarra changes

---
+++ meelad/assets/api.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+import joblib
+
+# # Load the model from the file
+model = joblib.load('random_forest_pregnancy_risk_model.pkl')
+
+# app = Flask(__name__)
+
+# @app.route('/api', methods=['GET'])
+# def predict():
+#     Get query parameters from the URL
+#     print ("ARgs:",request.args)
+#     print (request)
+#     age = float(request.args.get('age'))
+#     systolicbp = float(request.args.get('systolicbp'))
+#     diastolicbp = float(request.args.get('diastolicbp'))
+#     bs = float(request.args.get('bs'))
+#     bodytemp = float(request.args.get('bodytemp'))
+#     heartrate = float(request.args.get('heartrate'))
+
+#     result = model.predict([[age, systolicbp, diastolicbp, bs, bodytemp, heartrate]])
+#     print(result);
+#     return jsonify(result)
+    
+
+# if __name__ == "__main__":
+#     app.run()
+    
+# from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    
+    print ("ARgs:",request.args)
+    print (request)
+    age = float(request.args.get('age'))
+    systolicbp = float(request.args.get('systolicbp'))
+    diastolicbp = float(request.args.get('diastolicbp'))
+    bs = float(request.args.get('bs'))
+    bodytemp = float(request.args.get('bodytemp'))
+    heartrate = float(request.args.get('heartrate'))
+
+    result = model.predict([[age, systolicbp, diastolicbp, bs, bodytemp, heartrate]])
+    print(result);
+    return jsonify(result)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
