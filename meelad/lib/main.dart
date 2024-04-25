e62f4c75727d577325eb6f7dd9d4665d97074b3b
BaraaFatimaZohraLALAGUI <lalaguibaraa@gmail.com>
2024-04-25 04:16:42 +0000

    splashscreen and chooserole

--- meelad/lib/main.dart
+++ meelad/lib/main.dart
@@ -1,72 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:meelad/screens/chooserole.dart';
 import 'package:meelad/screens/landingpage.dart';
-import 'package:meelad/widgets/textfield.dart';
-import 'package:meelad/widgets/uploadfile.dart';
+import 'package:meelad/screens/signupgeny.dart';
+import 'package:meelad/screens/splashscreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meelad',
+      title: 'MeelaD',
       theme: ThemeData(),
-      home: const Landingpage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/chooserole': (context) => RoleChoice(),
+        // '/landingpage': (context) => Landingpage(),
+        '/signup': (context) => SignupGeny(),
+      },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late TextEditingController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = TextEditingController();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meelad'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Use buildTextField function from textfield.dart
-            buildTextField(controller, 'رقم الهاتف', '+21300000000', 'assets/icons/phone.png'),
-            const SizedBox(height: 20),
-            const Text('Other widgets go here'),
-            // Use buildUploadDocument function from uploadfile.dart
-            buildUploadDocument('hint', 'label', 'docs/files', (bool success) {
-              if (success) {
-                print('File Uploaded successfully');
-              } else {
-                print('failed to upload file');
-              }
-            }),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
   }
 }
